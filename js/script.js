d0b06ab87fec31830e802ba8aaf0f404e30d2252
Maksym Kuleshov <maksym_kuleshov@epam.com>
2021-12-05 22:44:55 +0200

    almost done without responsive

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('.game__slider').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	fade: true,
+	dots: true
+});
+
+$('.token__slider').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	fade: true,
+	dots: true
+});
