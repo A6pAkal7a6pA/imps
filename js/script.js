a063fa34cf1a9a72306652881ab004c5565ab834
Maksym Kuleshov <maksym_kuleshov@epam.com>
2021-12-09 17:46:10 +0200

    some fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 var gameSlider = $('.game__slider').slick({
 	slidesToShow: 1,
 	slidesToScroll: 1,
@@ -14,4 +15,13 @@ var tokenSlider = $('.token__slider').slick({
 	dots: true,
 	autoplay: true,
 	autoplaySpeed: 5000
+});
+
+$(document).ready(function () {
+	$('a[href^="#"]').click(function(){ // #1
+		let anchor = $(this).attr('href');  // #2
+		$('html, body').animate({           // #3
+		scrollTop:  $(anchor).offset().top  // #4
+		}, 600);                            // #5
+		});
 });
