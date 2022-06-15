9b6f9539b33969f49c7643c925983c135660c64f
Maksym Kuleshov <maksym_kuleshov@epam.com>
2022-06-15 22:33:50 +0300

    changed team

--- js/script.js
+++ js/script.js
@@ -222,3 +222,16 @@ const reincarnationSlider = new Swiper('.reincarnation__items', {
 	breakpoints: {
 	}
 });
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
